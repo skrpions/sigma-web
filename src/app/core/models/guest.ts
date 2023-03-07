73ce285af7bb9cc3603cae07230f9df1bf2d58e6
NESTOR MARTINEZ C <nestor.martinez.c@uniautonoma.edu.co>
2023-03-07 00:19:51 -0500

    refactor: ajustes en el html

--- src/app/core/models/guest.ts
+++ src/app/core/models/guest.ts
@@ -1,2 +1,6 @@
 export interface Guest {
+  department: string;
+  city: string;
+  name: string;
+  email: string;
 }
--- src/app/routes/guest/components/body/body.component.ts
+++ src/app/routes/guest/components/body/body.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { Department } from '../../../../core/models/department';
 import { DepartmentService } from '../../../../core/services/department.service';
+import { Guest } from './../../../../core/models/guest';
 
 @Component({
   selector: 'app-body',
@@ -11,139 +12,135 @@ import { DepartmentService } from '../../../../core/services/department.service'
 })
 export class BodyComponent {
 
-    formGuest!: FormGroup;
-    departments: Department[] = [];
-    cities: string[] = [];
+  formGuest!: FormGroup;
+  departments: Department[] = [];
+  cities: string[] = [];
+  listGuest: Guest[] = [];
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private _departmentSvc: DepartmentService
-    ) {
-        this.buildForm();
+  constructor(
+    private formBuilder: FormBuilder,
+    private _departmentSvc: DepartmentService
+  ) {
+    this.buildForm();
+  }
+
+  private buildForm() {
+
+    this.formGuest = this.formBuilder.group
+      ({
+        department: ['', [Validators.required, Validators.maxLength(30)]],
+        city: ['', [Validators.required, Validators.maxLength(50)]],
+        name: ['', [Validators.required, Validators.maxLength(50)]],
+        email: ['', [Validators.required, Validators.maxLength(30), Validators.email]]
+      });
+
+  }
+
+  ngOnInit(): void {
+
+    this.getDepartments();
+
+  }
+
+  private getDepartments(): void {
+
+    try {
+
+      // Consumo de API REST :: Obtengo los datos de la url
+      this._departmentSvc.getDepartamentos().subscribe(Departments => {
+        //console.log('Departments:', Departments);
+        //console.log('✅ Object.entries(Departments) ', Object.entries(Departments));
+        this.loadDepartments(Object.entries(Departments));
+      });
+
+    } catch (error) {
+      console.log('Error: ', error);
     }
 
-    private buildForm() {
+  }
 
-        this.formGuest = this.formBuilder.group
-            ({
-                department: ['', [Validators.required, Validators.maxLength(30)]],
-                city: ['', [Validators.required, Validators.maxLength(50)]],
-                name: ['', [Validators.required, Validators.maxLength(50)]],
-                email: ['', [Validators.required, Validators.maxLength(30), Validators.email]]
-            });
+  private loadDepartments(allDepartments: any[]): void {
 
-    }
+    allDepartments.map((department: any) => {
+      //console.log('👉 department: ', department);
 
-    ngOnInit(): void {
+      this.departments.push({
+        Department: department[0],
+        Cities: department[1]
+      });
 
-        this.getDepartments();
+    });
 
-    }
-
-    private getDepartments(): void {
-
-        try {
-
-            // Consumo de API REST :: Obtengo los datos de la url
-          this._departmentSvc.getDepartamentos().subscribe(Departments => {
-              //console.log('Departments:', Departments);
-              //console.log('✅ Object.entries(Departments) ', Object.entries(Departments));
-              this.loadDepartments(Object.entries(Departments));
-            });
-
-        } catch (error) {
-            console.log('Error: ', error);
-        }
-
-    }
-
-    private loadDepartments(allDepartments: any[]): void {
-
-        allDepartments.map((department: any) => {
-          console.log('👉 department: ', department);
-
-            this.departments.push({
-                Department: department[0],
-                Cities: department[1]
-            });
-
-        });
-
-      console.log('😉 this.departments: ', this.departments);
-    }
+    //console.log('😉 this.departments: ', this.departments);
+  }
 
   public changeDepartment(department: any): void {
 
     department = department.target.value;
 
-      try {
-            department === ''
-                ?
-                (
-                    this.formGuest.get('city')?.reset(''),
-                    this.cities = []
-                )
-                : this.cities = this.departments.find(elemento => elemento.Department === department)?.Cities ?? [];
+    try {
+      department === ''
+        ?
+        (
+          this.formGuest.get('city')?.reset(''),
+          this.cities = []
+        )
+        : this.cities = this.departments.find(elemento => elemento.Department === department)?.Cities ?? [];
 
-        } catch (error) {
-            console.log('Error: ', error);
-        }
+    } catch (error) {
+      console.log('Error: ', error);
     }
+  }
 
-    public send(): void {
+  public send(): void {
 
-      if (this.formGuest.valid) {
+    if (this.formGuest.invalid) return this.formGuest.markAllAsTouched(); // Activo todos los errores en el formGuest
 
-        this.sweetAlert();
+    const guest: Guest = this.formGuest.value;
+    this.listGuest.push(guest);
+    this.sweetAlert();
 
-        console.log('Valid:', this.formGuest.value);
+    console.log('✅ list: ', this.listGuest);
 
+    /* this._contactoSvc.create(contacto).subscribe((response: any) => {
 
-            // Obtenfo todos valores del formGuest
-            const contacto = this.formGuest.value;
+        // Condicional de corto circuito
+        response.success === true && this.sweetAlert();
 
-            /* this._contactoSvc.create(contacto).subscribe((response: any) => {
+    }); */
 
-                // Condicional de corto circuito
-                response.success === true && this.sweetAlert();
+  }
 
-            }); */
+  sweetAlert(): void {
 
-        } else {
-            // Activo todos los errores en el formGuest
-            this.formGuest.markAllAsTouched();
-        }
+    // Animación de Confirmación
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Datos Almacenados Exitosamente!',
+      showConfirmButton: false,
+      timer: 2200
+    });
 
-    }
+  }
 
-    sweetAlert(): void {
+  // - Recuperar los campos para que no sea tan repetitiva el llamado a dichos campos cuando trabaje con los errores
+  get departmentField() {
+    console.log('👉',  this.formGuest.get('department'));
 
-        // Animación de Confirmación
-        Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'Datos Almacenados Exitosamente!',
-            showConfirmButton: false,
-            timer: 2200
-        });
+    return this.formGuest.get('department');
+  }
 
-    }
+  get cityField() {
+    return this.formGuest.get('city');
+  }
 
-    // - Recuperar los campos para que no sea tan repetitiva el llamado a dichos campos cuando trabaje con los errores
-    get departmentField() {
-        return this.formGuest.get('department');
-    }
+  get nameField() {
+    return this.formGuest.get('name');
+  }
 
-    get cityField() {
-        return this.formGuest.get('city');
-    }
-
-    get nameField() {
-        return this.formGuest.get('name');
-    }
-
-    get emailField() {
-        return this.formGuest.get('email');
-    }
+  get emailField() {
+    return this.formGuest.get('email');
+  }
 
 }
